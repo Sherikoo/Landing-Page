d45e5cb219a2333ca9ed5ae769208b02203bb9b0
Mohamed Omar <72326565+Sherikoo@users.noreply.github.com>
2021-09-23 21:31:57 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,23 +1,24 @@
 /**
- * 
+ *
  * Manipulating the DOM exercise.
  * Exercise programmatically builds navigation,
  * scrolls to anchors from navigation,
  * and highlights section in viewport upon scrolling.
- * 
+ *
  * Dependencies: None
- * 
+ *
  * JS Version: ES2015/ES6
- * 
+ *
  * JS Standard: ESlint
- * 
+ *
 */
 
 /**
  * Define Global Variables
- * 
+ *
 */
-const sections = document.querySelectorAll("section");
+
+const sections = document.querySelectorAll('section');
 let navbar = document.querySelector("#navbar__list");
 const divs = document.querySelectorAll(".landing__container");
 const header = document.querySelector(".page__header");
@@ -26,8 +27,9 @@ const backToTopButton = document.querySelector("#top");
 /**
  * End Global Variables
  * Start Helper Functions
- * 
+ *
 */
+
 function deactivateAllNavbarItems() {
 	const navbarItems = document.querySelectorAll(".navbar_item");
 	for(let navbarItem of navbarItems) {
@@ -39,17 +41,6 @@ function deactivateAllSections() {
 	for(let section of sections) {
 		section.classList.remove("your-active-class");
 	}
-}
-
-function isInViewport(section) {
-    const rect = section.getBoundingClientRect();
-    return (
-        rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-
-    );
 }
 
 function getSectionOfNavbarItem(navbarItem) {
@@ -66,20 +57,30 @@ function getNavbarItemOfSection(section) {
     }
 }
 
+function isInViewport(section) {
+    const rect = section.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+
+    );
+}
 
 /**
  * End Helper Functions
  * Begin Main Functions
- * 
+ *
 */
 
 //build the nav
 function buildNavbar() {
-	for(const section of sections) {
-	let listItem = document.createElement("li");
-	listItem.innerHTML = `<a href="#${section.getAttribute("id")}" class="navbar_item">${section.getAttribute("data-nav")}</a>`;
-	navbar.appendChild(listItem);
-    }
+  for(const section of sections) {
+    let listItem = document.createElement("li");
+    listItem.innerHTML = `<a href="#${section.getAttribute('id')}" class="navbar_item">${section.getAttribute('data-nav')}</a>`;
+    navbar.appendChild(listItem);
+  }
 }
 
 // Add class 'your-active-class' to section when near top of viewport
@@ -89,31 +90,7 @@ function activateSection(section) {
 
 // Add class 'activeNavbarItem' to clicked or corresponding navbar link of section in viewport
 function activateNavbarItem(navbarItem) {
-	navbarItem.classList.add("activeNavbarItem");
-}
-
-// Funtion to hide navbar on scroll
-function hideNavbarOnScrolling() {
-	window.clearTimeout();
-	setTimeout(function() {
-		header.className = "page__header show";
-	}, 750);
-	header.className = "page__header hide";
-}
-
-// Function to scroll to top
-function scrollToTop() {
-	window.scrollTo({ top: 0, behavior: 'smooth' });
-}
-
-// Hide and show back-to-top button
-function hideAndShowBackToTopButton() {
-	if (window.scrollY > 0) {
-      	backToTopButton.className = "show";
-    } 
-    else {
-      	backToTopButton.className = "hide";
-    }
+  navbarItem.classList.add("activeNavbarItem");
 }
 
 // Paragraphs collapse on click
@@ -128,31 +105,55 @@ function paragraphCollapse(e, div) {
     }
 }
 
+// Hide and show back-to-top button
+function hideAndShowBackToTopButton() {
+	if (window.scrollY > 200) {
+      	backToTopButton.className = "show";
+    }
+    else {
+      	backToTopButton.className = "hide";
+    }
+}
+
+// Function to scroll to top
+function scrollToTop() {
+	window.scrollTo({ top: 0, behavior: 'smooth' });
+}
+
+// Funtion to hide navbar on scroll
+// function hideNavbarOnScrolling() {
+// 	window.clearTimeout();
+// 	setTimeout(function() {
+// 		header.className = "page__header show";
+// 	}, 750);
+// 	header.className = "page__header hide";
+// }
+
 
 /**
  * End Main Functions
  * Begin Events
- * 
+ *
 */
 
-// Build menu 
+// Build menu
 buildNavbar();
 
-// Scroll to section on link click
-const navbarItems = document.querySelectorAll(".navbar_item");
+// Scroll to section on navbar item click
+const navbarItems = document.querySelectorAll('.navbar_item');
 for(let navbarItem of navbarItems) {
-	navbarItem.addEventListener("click", function(e) {
-		e.preventDefault();
-		deactivateAllNavbarItems();
-		deactivateAllSections();
-		activateNavbarItem(navbarItem);
-		const sectionToView = getSectionOfNavbarItem(navbarItem);
+  navbarItem.addEventListener('click', function(e) {
+    e.preventDefault();
+    deactivateAllNavbarItems();
+    deactivateAllSections();
+    activateNavbarItem(navbarItem);
+    const sectionToView = getSectionOfNavbarItem(navbarItem);
 		activateSection(sectionToView);
-		sectionToView.scrollIntoView({behavior: "smooth"});
-	});
+    sectionToView.scrollIntoView({behavior: "smooth"});
+  })
 }
 
-// Set sections as active
+// Set sections as active on scroll
 window.addEventListener("scroll", function() {
 	for(let section of sections) {
 		if(isInViewport(section)){
@@ -163,7 +164,6 @@ window.addEventListener("scroll", function() {
 		}
 	}
 });
-
 
 // Paragraphs collapse on click
 for(let div of divs) {
@@ -177,5 +177,6 @@ window.addEventListener('scroll', hideAndShowBackToTopButton);
 
 backToTopButton.addEventListener("click", scrollToTop);
 
+
 // Hide Navbar while when user stops scrolling
-window.addEventListener("scroll", hideNavbarOnScrolling);
+// window.addEventListener("scroll", hideNavbarOnScrolling);
